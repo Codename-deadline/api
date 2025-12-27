5553cacd60c9f37eb3c7ebe3c1a10e8be4313217
Nikita Astafyev <nikita.astafyev@gmail.com>
2025-12-27 19:38:48 +0700

    fix: serialize ErrorCode as value

--- src/main/kotlin/xyz/om3lette/deadlines_api/util/GeneralErrorResponse.kt
+++ src/main/kotlin/xyz/om3lette/deadlines_api/util/GeneralErrorResponse.kt
@@ -1,15 +1,17 @@
 package xyz.om3lette.deadlines_api.util
 
+import com.fasterxml.jackson.annotation.JsonInclude
 import org.springframework.web.ErrorResponse
 import xyz.om3lette.deadlines_api.exceptions.enums.ErrorCode
 import xyz.om3lette.deadlines_api.exceptions.type.StatusCodeException
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 data class GeneralErrorResponse(
     val code: ErrorCode,
 
     val detail: String? = null,
 
-    val params: Map<String, Any> = emptyMap()
+    val params: Map<String, Any>? = null
 ) {
     companion object {
         fun fromStatusCodeException(error: StatusCodeException): GeneralErrorResponse =
