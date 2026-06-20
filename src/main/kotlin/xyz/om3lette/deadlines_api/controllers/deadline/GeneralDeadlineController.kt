cb38cc1795e8d3d5478370bcc604bc2307388be3
Nikita Astafyev <nikita.astafyev@gmail.com>
2026-06-20 21:28:13 +0300

    fix: exposed user deadlines endpoint

---
+++ src/main/kotlin/xyz/om3lette/deadlines_api/controllers/deadline/GeneralDeadlineController.kt
@@ -0,0 +1,27 @@
+package xyz.om3lette.deadlines_api.controllers.deadline
+
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.security.SecurityRequirement
+import io.swagger.v3.oas.annotations.tags.Tag
+import org.springframework.security.core.annotation.AuthenticationPrincipal
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+import xyz.om3lette.deadlines_api.data.user.model.User
+import xyz.om3lette.deadlines_api.services.DeadlineService
+
+@SecurityRequirement(name = "bearerAuth")
+@RestController
+@RequestMapping("/api/deadlines")
+@Tag(name = "Deadlines")
+class GeneralDeadlineController(
+    private val deadlineService: DeadlineService,
+) {
+    @GetMapping("/me")
+    @Operation(summary = "Get deadlines which are assigned to you")
+    fun getUserDeadlines(
+        @AuthenticationPrincipal user: User,
+        @RequestParam("page") pageNumber: Int
+    ) = deadlineService.getDeadlinesByUser(user, pageNumber, 10)
+}
