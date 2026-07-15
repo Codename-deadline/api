8044286328a7273dd226a7c7e8a4f723844c94a8
Nikita Astafyev <nikita.astafyev@gmail.com>
2026-07-15 13:32:52 +0700

    build: ssl config

--- src/main/kotlin/xyz/om3lette/deadlines_api/configs/KafkaProducerConfig.kt
+++ src/main/kotlin/xyz/om3lette/deadlines_api/configs/KafkaProducerConfig.kt
@@ -1,6 +1,8 @@
 package xyz.om3lette.deadlines_api.configs
 
+import org.apache.kafka.clients.CommonClientConfigs
 import org.apache.kafka.clients.producer.ProducerConfig
+import org.apache.kafka.common.config.SslConfigs
 import org.apache.kafka.common.serialization.StringSerializer
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -19,11 +21,30 @@ class KafkaProducerConfig(
     kafkaProperties: KafkaProperties
 ) {
 
-    private val configProps: MutableMap<String, Any> = mutableMapOf(
-        ProducerConfig.BOOTSTRAP_SERVERS_CONFIG  to kafkaProperties.bootstrapServers,
-        ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG to true,
-        ProducerConfig.COMPRESSION_TYPE_CONFIG   to "gzip",
-    )
+    private val configProps: Map<String, Any> = buildMap {
+        put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaProperties.bootstrapServers)
+        put(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, true)
+        put(ProducerConfig.COMPRESSION_TYPE_CONFIG, "gzip")
+        put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, kafkaProperties.securityProtocol)
+
+        kafkaProperties.ssl.truststoreLocation?.takeIf(String::isNotBlank)?.let {
+            put(SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG, it)
+            put(SslConfigs.SSL_TRUSTSTORE_TYPE_CONFIG, "PKCS12")
+        }
+        kafkaProperties.ssl.truststorePassword?.takeIf(String::isNotBlank)?.let {
+            put(SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG, it)
+        }
+        kafkaProperties.ssl.keystoreLocation?.takeIf(String::isNotBlank)?.let {
+            put(SslConfigs.SSL_KEYSTORE_LOCATION_CONFIG, it)
+            put(SslConfigs.SSL_KEYSTORE_TYPE_CONFIG, "PKCS12")
+        }
+        kafkaProperties.ssl.keystorePassword?.takeIf(String::isNotBlank)?.let {
+            put(SslConfigs.SSL_KEYSTORE_PASSWORD_CONFIG, it)
+        }
+        kafkaProperties.ssl.keyPassword?.takeIf(String::isNotBlank)?.let {
+            put(SslConfigs.SSL_KEY_PASSWORD_CONFIG, it)
+        }
+    }
 
     @Bean
     fun notificationProducerFactory(): ProducerFactory<String, DeadlineNotificationEvent> =
--- src/main/kotlin/xyz/om3lette/deadlines_api/configs/properties/KafkaProperties.kt
+++ src/main/kotlin/xyz/om3lette/deadlines_api/configs/properties/KafkaProperties.kt
@@ -1,5 +1,6 @@
 package xyz.om3lette.deadlines_api.configs.properties
 
+import jakarta.validation.Valid
 import jakarta.validation.constraints.NotBlank
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.validation.annotation.Validated
@@ -8,5 +9,19 @@ import org.springframework.validation.annotation.Validated
 @ConfigurationProperties("spring.kafka")
 data class KafkaProperties(
     @field:NotBlank
-    val bootstrapServers: String = "localhost:9092"
-)
+    val bootstrapServers: String = "localhost:9092",
+
+    @field:NotBlank
+    val securityProtocol: String = "PLAINTEXT",
+
+    @field:Valid
+    val ssl: Ssl = Ssl()
+) {
+    data class Ssl(
+        val truststoreLocation: String? = null,
+        val truststorePassword: String? = null,
+        val keystoreLocation: String? = null,
+        val keystorePassword: String? = null,
+        val keyPassword: String? = null
+    )
+}
