d2dabf9ffddd8625643e4031632cfe7457961dea
Nikita Astafyev <nikita.astafyev@gmail.com>
2026-07-21 15:35:13 +0700

    ops: add Status.INTERNAL and unhandled exceptions logging in gRPC exception handler

--- src/main/kotlin/xyz/om3lette/deadlines_api/exceptions/handlers/GlobalGrpcExceptionHandler.kt
+++ src/main/kotlin/xyz/om3lette/deadlines_api/exceptions/handlers/GlobalGrpcExceptionHandler.kt
@@ -3,17 +3,20 @@ package xyz.om3lette.deadlines_api.exceptions.handlers
 import io.grpc.Metadata
 import io.grpc.StatusException
 import io.grpc.Status
+import org.slf4j.LoggerFactory
 import org.springframework.dao.DataIntegrityViolationException
 import org.springframework.grpc.server.exception.GrpcExceptionHandler
 import org.springframework.stereotype.Component
 import xyz.om3lette.deadlines_api.exceptions.type.GrpcKeyLocaleException
 import xyz.om3lette.deadlines_api.exceptions.type.StatusCodeException
 import xyz.om3lette.deadlines_api.proto.Locale
+import xyz.om3lette.deadlines_api.services.integration.IntegrationInternalService
 
 
 @Component
 class GlobalGrpcExceptionHandler : GrpcExceptionHandler {
     private val localeKey = Metadata.Key.of("locale", Metadata.ASCII_STRING_MARSHALLER)
+    private val logger = LoggerFactory.getLogger(IntegrationInternalService::class.java)
 
     override fun handleException(exception: Throwable): StatusException {
         return when (exception) {
@@ -32,12 +35,16 @@ class GlobalGrpcExceptionHandler : GrpcExceptionHandler {
                     .asException(metadata)
             }
             is GrpcKeyLocaleException -> {
+                if (exception.status == Status.INTERNAL) {
+                    logger.error("Internal error while processing gRPC request: ", exception)
+                }
                 val metadata = Metadata().apply { put(localeKey, exception.locale.name) }
                 exception.status
                     .withDescription(exception.key)
                     .asException(metadata)
             }
             else -> {
+                logger.error("Unhandled exception while processing gRPC request: ", exception)
                 val metadata = Metadata().apply { put(localeKey, Locale.EN.name) }
                 Status.INTERNAL
                     .withDescription("errors.server_internal")
